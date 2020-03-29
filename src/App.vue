d00609f2594fda9381d338809e7d26c45842f5de
huodada <1015428987@qq.com>
2020-03-29 18:23:05 +0800

    finish clear

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        wojinxingle yici genggai 
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Test/>>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './views/Test'
-
 
 export default {
   name: 'app',
