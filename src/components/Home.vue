39f507517844d073d9a822bb2ebd7ff46a18b4bc
huodada <1015428987@qq.com>
2020-04-02 16:49:59 +0800

    修改已知弹窗问题

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,35 @@
 <template>
-  <div class="page">
-      <h1>home组件</h1>
-      <el-button type="info" @click="loginout">退出</el-button>
-  </div>
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header class="info">
+      <div class="page">
+        <h1>home组件</h1>
+        <el-button type="info" @click="loginout">退出</el-button>
+      </div>
+    </el-header>
+    <!-- 页面主体 -->
+    <el-container>
+      <!-- 主体侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主题 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
   data() {
-    return {
-
-    }
+    return {};
   },
-  components: {
-
-  },
+  components: {},
   methods: {
-    loginout(){
-      window.sessionStorage.clear()
-      this.$router.push('/login')
+    loginout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
     }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,13 +4,23 @@ import {
   Form,
   FormItem,
   Input,
-  Message
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
 } from 'element-ui'
-// 插件注册
+// 注册全剧组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Message)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
 // 全局挂载
 Vue.prototype.$message = Message
